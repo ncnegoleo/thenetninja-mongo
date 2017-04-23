77892930819ee8033a22ec6e5a985e90e0397d3e
Leonardo Soares <leonardo.nightcrawler@gmail.com>
2017-04-22 22:52:35 -0300

    Mongo tests with mocha started

---
+++ test/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+// Connect to database before tests run
+before(function (done) {
+
+    // Connect to MongoDB
+    mongoose.connect('mongodb://localhost/testaroo');
+
+    mongoose.connection.once('open', function () {
+        console.log('Connection has been made, now make fireworks...');
+        done();
+    }).on('error', function (error) {
+        console.log('Connection error');
+    });
+});
+
